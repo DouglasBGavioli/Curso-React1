a0d138d1739ebeb1c2b311d68eec0e77a6c6e0a3
Douglas Biassi Gavioli <douglasbgavioli@hotmail.com>
2021-08-16 15:24:37 -0300

    Alteracoes com o curso 2

---
+++ src/components/ListaDeCategorias/ListaDeCategorias.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import "./estilo.css";
+
+class ListaDeCategorias extends Component {
+  constructor(){
+    super();
+    this.state = {categorias:[]}
+
+    this._novasCategorias = this._novasCategorias.bind(this);
+  }
+  
+  
+  componentDidMount() {
+    this.props.categorias.inscrever(this._novasCategorias);
+  }
+
+  componentWillUnmount(){
+    this.props.categorias.desinscrever(this._novasCategorias);
+  }
+
+  _novasCategorias(categorias) {
+    this.setState({...this.state,categorias})
+  }
+
+  _hendleEventoInput(e) {
+    if (e.key === "Enter") {
+      let valorCategoria = e.target.value;
+      this.props.adicionarCategoria(valorCategoria);
+    }
+  }
+  render() {
+    return (
+      <section className="lista-categorias">
+        <ul className="lista-categorias_lista">
+          {this.state.categorias.map((categoria, index) => {
+            return (
+              <li key={index} className="lista-categorias_item">
+                {categoria}
+              </li>
+            );
+          })}
+        </ul>
+        <input
+          type="text"
+          className="lista-categorias_input"
+          placeholder="Adicionar categoria"
+          onKeyUp={this._hendleEventoInput.bind(this)}
+        />
+      </section>
+    );
+  }
+}
+
+export default ListaDeCategorias;
